5cda9a392f0391f865f6329652974ecab790732b
Vitor <vitorabsilva10@gmail.com>
2025-02-23 16:04:26 -0300

    mod10 refactor

--- lib/dart_basic/exercises/e5_mod10.dart
+++ lib/dart_basic/exercises/e5_mod10.dart
@@ -15,58 +15,72 @@ do item 4. Se os números forem iguais, o cartão é valido, e se forem diferent
 
 */
 
-//5419 8250 0346 1210 - Inválido
-//4916 6418 5936 9080 - Válido
 void main() {
-  print(validateCreditCard(4916641859369080));
-  print(validateCreditCard(5419825003461210));
+  //Valid
+  final CreditCard card = CreditCard(creditCardNumber: '4916641859369080');
+  final CreditCard card2 = CreditCard(creditCardNumber: '491.664185-936,90.80');
+
+  //Invalid
+  final CreditCard card3 = CreditCard(creditCardNumber: '5419825003461210');
+  final CreditCard card4 = CreditCard(creditCardNumber: '541.982.500.346.1210');
+  final CreditCard card5 = CreditCard(creditCardNumber: '49122226641859369080');
+
+  print(card.validateCreditCard());
+  print(card2.validateCreditCard());
+  print(card3.validateCreditCard());
+  print(card4.validateCreditCard());
+  print(card5.validateCreditCard());
 }
 
-bool validateCreditCard(int cardNumber) {
-  final List<int> split =
-      cardNumber.toString().split('').map(int.parse).toList();
+class CreditCard {
+  final String creditCardNumber;
 
-  final lastDigit = split[split.length - 1];
-  print('lastDigit $lastDigit');
+  CreditCard({required this.creditCardNumber});
 
-  //Multiplicar numero por numero
-  //COLUNA impar por 1 e par por 2
-  //Valor >= 10 separar numero e somar.
-  //Final somar todos os valores
+  bool validateCreditCard() {
+    final List<int> digitsList = creditCardNumberParse();
+    final isCreditCardNumberValid = creditCardNumberIsValid(digitsList);
+    if (isCreditCardNumberValid) {
+      final lastDigit = digitsList[digitsList.length - 1];
 
-  var index = 0;
-  final sub = split.sublist(0, 15);
-  final multipliedList = sub.map((e) {
-    final bool isEven = index % 2 == 0;
-    final mult = isEven ? e * 2 : e * 1;
-    index++;
-    return numberParse(mult);
-  }).toList();
+      var index = 0;
+      final digitsWithoutLast = digitsList.sublist(0, 15);
+      final multipliedList = digitsWithoutLast.map((e) {
+        final bool isEven = index % 2 == 0;
+        final mult = isEven ? e * 2 : e * 1;
+        index++;
+        return numberParse(mult);
+      }).toList();
 
-  final listSum = multipliedList.reduce((a, b) => a + b);
-  print(listSum);
+      final listSum = multipliedList.reduce((a, b) => a + b);
+      final rest = listSum % 10;
+      final validationDigit = rest == 0 || rest == 10 ? 0 : 10 - rest;
 
-  //3. Com o resultado anterior, faça uma divisão por 10 para obter o resto da divisão:
-  final rest = listSum % 10;
+      return validationDigit == lastDigit;
+    } else {
+      return false;
+    }
+  }
 
-  //4. Subtraia o resto da divisão por 10:
-  //Se o resto for 0 esse é o dígito
-  // Subtrait 10 - resto
-  final validationDigit = rest == 0 || rest == 10 ? 0 : 10 - rest;
+  List<int> creditCardNumberParse() {
+    return creditCardNumber
+        .split('')
+        .where((char) => '0123456789'.contains(char))
+        .map(int.parse)
+        .toList();
+  }
 
-  /*
-  5. Faça a verificação do número que foi separado no item 1 com o resultado
-  do item 4. Se os números forem iguais, o cartão é valido, e se forem diferentes, é invalido.
-  */
-  return validationDigit == lastDigit;
-}
+  bool creditCardNumberIsValid(List<int> cpfList) {
+    return cpfList.length == 16;
+  }
 
-int numberParse(value) {
-  if (value >= 10) {
-    final aux = value.toString().split('').map(int.parse).toList();
-    final auxSum = aux[0] + aux[1];
-    return auxSum >= 10 ? numberParse(auxSum) : auxSum;
-  } else {
-    return value;
+  int numberParse(value) {
+    if (value >= 10) {
+      final aux = value.toString().split('').map(int.parse).toList();
+      final auxSum = aux[0] + aux[1];
+      return auxSum >= 10 ? numberParse(auxSum) : auxSum;
+    } else {
+      return value;
+    }
   }
 }
