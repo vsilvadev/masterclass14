caeb5dc7b6f53d1e85effa959949bd43ea07d8e2
Vitor <vitorabsilva10@gmail.com>
2025-02-23 15:24:38 -0300

    imc exercise refactor

--- lib/dart_basic/exercises/e1_imc.dart
+++ lib/dart_basic/exercises/e1_imc.dart
@@ -7,12 +7,19 @@ Create a function that performs the BMI calculation.
 */
 
 void main() {
-  final bmi = calculateBMI(100, 1.80);
-  print(bmi);
+  final person = Person(weight: 100, height: 1.80);
+  print(person.calculateBMI());
 }
 
-double calculateBMI(double weight, double height) {
-  final cmHeight = height > 2.52 ? height / 100 : height;
-  return double.parse((weight / (cmHeight * cmHeight)).toStringAsFixed(1));
-  //pow(cmHeight, 2) could have been used to calculate the power
+class Person {
+  final double weight;
+  final double height;
+
+  Person({required this.weight, required this.height});
+
+  double calculateBMI() {
+    final cmHeight = height > 2.52 ? height / 100 : height;
+    return double.parse((weight / (cmHeight * cmHeight)).toStringAsFixed(1));
+    //pow(cmHeight, 2) could have been used to calculate a numer power
+  }
 }
